9c8f8dd8b553ad280447753d223d91d61e7346ed
Nathan Harris <nathanh@l4digital.com>
2017-12-28 17:25:22 -0800

    Add Carthage framework support

---
+++ Example/Pods/SegmentLocalyticsIntegration/SegmentLocalyticsIntegration.h
@@ -0,0 +1,17 @@
+//
+//  SegmentLocalyticsIntegration.h
+//  SegmentLocalyticsIntegration
+//
+//  Created by nathanh on 12/28/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SegmentLocalyticsIntegration.
+FOUNDATION_EXPORT double SegmentLocalyticsIntegrationVersionNumber;
+
+//! Project version string for SegmentLocalyticsIntegration.
+FOUNDATION_EXPORT const unsigned char SegmentLocalyticsIntegrationVersionString[];
+
+#import <SegmentLocalyticsIntegration/SEGLocalyticsIntegration.h>
+#import <SegmentLocalyticsIntegration/SEGLocalyticsIntegrationFactory.h>
